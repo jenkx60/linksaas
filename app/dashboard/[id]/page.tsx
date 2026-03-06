df21ce4c59f2b87548cf9e5ba6dd09ea5de957af
jenkx60 <jenkinsuwagbai18@gmail.com>
2026-03-06 13:11:43 +0100

    build error fix

---
+++ app/dashboard/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DashboardIdPage() {
+  return (
+    <div>
+      {/* Placeholder for dashboard detail page */}
+      <h1>Dashboard Details</h1>
+    </div>
+  );
+}
